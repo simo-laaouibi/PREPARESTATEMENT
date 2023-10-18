969b0ca8cca9f318dfd5ee74a4a38cf1e52876f0
simo-laaouibi <148088062+simo-laaouibi@users.noreply.github.com>
2023-10-18 19:15:36 +0100

    Add files via upload

---
+++ TP1/src/beans/Site.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author SIMO LAAOUIBI
+ */
+public class Site {
+private int id;
+private String nom;
+
+    public Site() {
+    }
+
+    public Site(String nom) {
+        this.nom = nom;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+}
